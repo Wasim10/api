593c569744ff47b381ba96774c9fe84a49fdec79
Waseem10 <mw82777@gmail.com>
2019-03-26 15:16:50 +0500

    Doing Rating & Discount Work in ProductResource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,11 +15,19 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return[
-            'Product_name' => $this->name,
+            'Product_name' => $this->name, 
             'Product_detail' => $this->detail,
             'Product_price' => $this->price,
             'Product_discount' => $this->discount,
-            'Product_stock' => $this->stock,
+            'Product_stock' => $this->stock == 0 ? 'Out of Stock, Please Check Later' : $this->stock,
+
+            'TotalPrice' => round((1-($this->discount/100)) * $this->price,2),
+
+            'Rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No One Rating Yet',
+
+            'href' => [
+                'reviewsLINK' => route('reviews.index', $this->id)
+            ]
            
  
         ];
